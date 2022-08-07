58001cab9cebddc94a5cd46ffd4b4191352564fc
azahar24 <azaharuddin454@gmail.com>
2022-08-07 15:02:18 +0600

    Initial commit

--- lib/ui/views/add_new_note.dart
+++ lib/ui/views/add_new_note.dart
@@ -8,17 +8,31 @@ class AddNewNote extends StatefulWidget {
 }
 
 class _AddNewNoteState extends State<AddNewNote> {
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _decController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.only(top: 15,left: 15,right: 15),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         color: Colors.white,
+
       ),
       child: Column(
         children: [
-          TextField(),
-          TextField(),
+          TextField(
+            controller: _titleController,
+            decoration: InputDecoration(
+              hintText: 'title'
+            ),
+          ),
+          TextField(
+            controller: _decController,
+            decoration: InputDecoration(
+                hintText: 'title'
+            ),
+          ),
         ],
       ),
     );
