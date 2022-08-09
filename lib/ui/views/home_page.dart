0c10771a751c708ede09e7072a73cb0fdefefdbe
azahar24 <azaharuddin454@gmail.com>
2022-08-09 15:57:37 +0600

    Initial commit

--- lib/ui/views/home_page.dart
+++ lib/ui/views/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:my_diary/ui/views/add_new_note.dart';
 
@@ -16,6 +18,7 @@ class _HomePageState extends State<HomePage> {
         builder: (context)=>AddNewNote()
     );
   }
+  final Stream<QuerySnapshot<Map<String, dynamic>>> _usersStream = FirebaseFirestore.instance.collection('notes').doc(FirebaseAuth.instance.currentUser!.email).collection('item').snapshots();
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +27,29 @@ class _HomePageState extends State<HomePage> {
         onPressed: ()=>addNewNote(),
         child: Icon(Icons.add,color: Colors.white,),
         //backgroundColor: Colors.white,
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: _usersStream,
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text('Something went wrong'));
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                return ListTile(
+                  title: Text(data['title']),
+                  subtitle: Text(data['dec']),
+                );
+              }).toList(),
+            );
+          }
+
       ),
     );
   }
